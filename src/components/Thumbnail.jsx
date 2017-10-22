f62c90c50c1df8896f0b7d7190cb4efceeb146a9
Dennis Ideler <ideler.dennis@gmail.com>
2017-10-22 12:04:12 +0100

    Allow Thumbnail alt to be overridden

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 import './Thumbnail.css';
 
-export default function Thumbnail({ src, width, selected, clickAction }) {
+export default function Thumbnail({ src, alt, width, selected, clickAction }) {
   return (
     <img
       src={src}
-      alt="Thumbnail in gallery"
+      alt={alt}
       className={`Thumbnail ${selected && 'ThumbnailSelected'}`}
       style={{ width: `${width}%` }}
       onClick={clickAction}
@@ -17,7 +17,12 @@ export default function Thumbnail({ src, width, selected, clickAction }) {
 
 Thumbnail.propTypes = {
   src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
   width: PropTypes.number,
   selected: PropTypes.bool,
   clickAction: PropTypes.func,
+};
+
+Thumbnail.defaultProps = {
+  alt: 'Thumbnail in gallery',
 };
